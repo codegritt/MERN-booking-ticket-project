03f65f84aef598d4454b2477effcbd8ed4b78ef5
gokul <gokulchandan18@gmail.com>
2023-07-02 00:13:41 +0530

    a

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,26 +4,17 @@ import nav__wrapper from "./Header.module.css";
 import navigation from "./Header.module.css";
 import Logo from "./Header.module.css";
 import menu from "./Header.module.css";
+import menua from "./Header.module.css";
 import nav_right from "./Header.module.css";
 import nav_buttons from "./Header.module.css";
 import logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
+import { RiBarChartHorizontalLine } from "react-icons/ri";
+import menuicon from "./Header.module.css";
+import travelLogo from "./Header.module.css";
 
 const Header = () => {
-  const navLinks = [
-    {
-      path: "/home",
-      display: "Home",
-    },
-    {
-      path: "/#",
-      display: "About",
-    },
-    {
-      path: "/tours",
-      display: "Tours",
-    },
-  ];
+ 
   return (
     <>
       <div className={headername.headername}>
@@ -31,18 +22,18 @@ const Header = () => {
           <div>
             <div className={nav__wrapper.nav__wrapper}>
               <div className={Logo.Logo}>
-                <img src={logo} alt="" />
+                <img className={travelLogo.travelLogo} src={logo} alt="" />
               </div>
 
               <div className={navigation.navigation}>
                 <div className={menu.menu}>
-                  <span style={{ marginRight: "40px" }}>
+                  <span className={menua.menua} style={{ marginRight: "40px" }}>
                     <Link to="/home">Home</Link>
                   </span>
-                  <span style={{ marginRight: "40px" }}>
+                  <span className={menua.menua} style={{ marginRight: "40px" }}>
                     <Link to="#">About</Link>
                   </span>
-                  <span>
+                  <span className={menua.menua}>
                     <Link to="/tours">Tours</Link>
                   </span>
                 </div>
@@ -50,13 +41,17 @@ const Header = () => {
 
               <div className={nav_right.nav_right}>
                 <div className={nav_buttons.nav_buttons}>
-                  <button type="button" class="btn btn-secondary">
+                  <button style={{borderRadius:"20px"}} type="button" class="btn btn-secondary">
                     <Link to="/login">Login</Link>
                   </button>
-                  <button type="button" class="btn btn-warning">
+                  <button  style={{borderRadius:"20px"}} type="button" class="btn btn-warning">
                     <Link to="/register">Register</Link>
                   </button>
                 </div>
+
+                <span className={menuicon.menuicon }>
+                <RiBarChartHorizontalLine/>
+                </span>
               </div>
             </div>
           </div>
